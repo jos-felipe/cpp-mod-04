ee234409e0839a14c241db55468fbaa980935f6b
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-11-08 18:49:53 -0300

    Implement AAnimal class with constructors, destructor, and type getter; remove Animal class

---
+++ ex02/AAnimal.cpp
@@ -0,0 +1,24 @@
+#include "AAnimal.hpp"
+
+AAnimal::AAnimal() : type("AAnimal") {
+    std::cout << "AAnimal default constructor called" << std::endl;
+}
+
+AAnimal::AAnimal(const AAnimal& other) {
+    std::cout << "AAnimal copy constructor called" << std::endl;
+    *this = other;
+}
+
+AAnimal& AAnimal::operator=(const AAnimal& other) {
+    if (this != &other)
+        this->type = other.type;
+    return *this;
+}
+
+AAnimal::~AAnimal() {
+    std::cout << "AAnimal destructor called" << std::endl;
+}
+
+std::string AAnimal::getType() const {
+    return type;
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,7 @@
+#include "AAnimal.hpp"
+
+int main(void) {
+	AAnimal* a = new AAnimal();
+
+	return 0;
+}
