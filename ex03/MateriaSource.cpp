bc12f0756adc333fd626122d86e718a6021a2f52
Felipe <josfelip@debian12>
2024-11-12 14:56:01 -0300

    Add MateriaSource class implementation

---
+++ ex03/MateriaSource.cpp
@@ -0,0 +1,60 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	for (int i = 0; i < MEMORY_SIZE; i++)
+		this->_memory[i] = NULL;
+}
+
+MateriaSource::MateriaSource(const MateriaSource& src)
+{
+	for (int i = 0; i < MEMORY_SIZE; i++)
+		this->_memory[i] = NULL;
+	*this = src;
+}
+
+MateriaSource&	MateriaSource::operator=(const MateriaSource& rhs)
+{
+	if (this != &rhs)
+	{
+		for (int i = 0; i < MEMORY_SIZE; i++)
+		{
+			delete this->_memory[i];
+			this->_memory[i] = NULL;
+			if (rhs._memory[i])
+				this->_memory[i] = rhs._memory[i]->clone();
+		}
+	}
+	return *this;
+}
+
+MateriaSource::~MateriaSource()
+{
+	for (int i = 0; i < MEMORY_SIZE; i++)
+		delete this->_memory[i];
+}
+
+void	MateriaSource::learnMateria(AMateria* m)
+{
+	for (int i = 0; i < MEMORY_SIZE; i++)
+	{
+		if (this->_memory[i] == NULL)
+		{
+			this->_memory[i] = m;
+			return;
+		}
+	}
+	std::cout << RED << "Memory is full, can't learn new materias. Materia input will be deleted." << RESET << std::endl;
+	delete m;
+}
+
+AMateria*	MateriaSource::createMateria(std::string const& type)
+{
+	for (int i = 0; i < MEMORY_SIZE; i++)
+	{
+		if (this->_memory[i] && type.compare(this->_memory[i]->getType()) == 0) 
+			return this->_memory[i]->clone();
+	}
+	std::cout << RED << type << " is an unkown type, failed to create materia" << RESET << std::endl;
+	return 0;
+}
---
+++ ex03/MateriaSource.hpp
@@ -0,0 +1,23 @@
+#ifndef MATERIA_SOURCE_HPP
+# define MATERIA_SOURCE_HPP
+
+# include "IMateriaSource.hpp"
+# include "Colors.hpp" 
+# define MEMORY_SIZE 4
+
+class	MateriaSource : public IMateriaSource
+{
+public:
+	MateriaSource();
+	MateriaSource(const MateriaSource& rhs);
+	MateriaSource& operator=(const MateriaSource& rhs);
+	~MateriaSource();
+
+	void	learnMateria(AMateria* m);
+	AMateria*	createMateria(std::string const& type);
+
+private:
+	AMateria*	_memory[MEMORY_SIZE];
+};
+
+#endif
