ec499d76365e835d5c3a6d53c7a1c06da4790502
Felipe <josfelip@debian12>
2024-11-12 11:51:01 -0300

    Complete the definition of the AMateria class

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -1,18 +1,23 @@
 #ifndef AMATERIA_HPP
 # define AMATERIA_HPP
 
+# include <iostream>
 # include <string>
+# include "ICharacter.hpp"
+# include "Colors.hpp"
 
-class AMateria
-{
+class AMateria {
 	protected:
-
+		std::string _type;
 
 	public:
+		AMateria();
 		AMateria(std::string const & type);
+		AMateria(AMateria const & src);
+		AMateria & operator=(AMateria const & rhs);
+		virtual ~AMateria();
 
-		std::string const & getType() const; // Returns the materia type
-
+		std::string const & getType() const;
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
 }
