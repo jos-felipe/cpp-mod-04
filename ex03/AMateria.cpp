d2fd5c01542ac488142829de9010950de7939d00
Felipe <josfelip@debian12>
2024-11-12 12:03:20 -0300

    Refactor AMateria class implementation

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -1,29 +1,22 @@
 #include "AMateria.hpp"
 
-// Constructor
+AMateria::AMateria() {}
+
 AMateria::AMateria(std::string const & type) : _type(type) {}
 
-// Copy Constructor
 AMateria::AMateria(AMateria const & other) : _type(other._type) {}
 
-// Destructor
-AMateria::~AMateria() {}
-
-// Assignment Operator
 AMateria & AMateria::operator=(AMateria const & other) {
-	if (this != &other) {
-		this->_type = other._type;
-	}
+	(void)other;
 	return *this;
 }
 
-// Get the type of the materia
+AMateria::~AMateria() {}
+
 std::string const & AMateria::getType() const {
 	return this->_type;
 }
 
-// Use the materia
 void AMateria::use(ICharacter & target) {
-	// Implementation of use function
-	// This should be overridden in derived classes
+	std::cout << "A generic Materia is trying to be used on " << target.getName() << std::endl;
 }
