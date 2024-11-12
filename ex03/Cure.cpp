eaf1490fbe769196181634daea07a94125768223
Felipe <josfelip@debian12>
2024-11-12 12:24:36 -0300

    Add Cure class implementation

---
+++ ex03/Cure.cpp
@@ -0,0 +1,20 @@
+#include "Cure.hpp"
+
+Cure::Cure() : AMateria("cure") {}
+
+Cure::Cure(const Cure &other) : AMateria(other) {}
+
+Cure &Cure::operator=(const Cure &other) {
+	(void)other;
+	return *this;
+}
+
+Cure::~Cure() {}
+
+AMateria* Cure::clone() const {
+	return new Cure(*this);
+}
+
+void Cure::use(ICharacter &target) {
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+}
